1a45ac1804243595466478c73ecef0e686f7ffd0
bobermcboberson <tf2spyman5@gmail.com>
2021-01-04 13:59:35 -0500

    Added Fields to Superclass

--- ComparingGameData/src/com/company/BestSelling.java
+++ ComparingGameData/src/com/company/BestSelling.java
@@ -2,4 +2,10 @@ package com.company;
 
 public class BestSelling {
 
+    // Fields
+    private String name;
+    private int unitSold;
+    private String releaseDate;
+    private String creator;
+
 }
