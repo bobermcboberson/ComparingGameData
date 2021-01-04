5ccfe97987ad650b4852eb908fa181b1795fb06d
fudaylhopkins <Hopkinsfudayl@gmail.com>
2021-01-04 14:02:59 -0500

    added super class

--- ComparingGameData/src/com/company/BestSellingConsole.java
+++ ComparingGameData/src/com/company/BestSellingConsole.java
@@ -1,5 +1,5 @@
 package com.company;
 
 public class BestSellingConsole extends BestSelling {
-
+int
 }
---
+++ ComparingGameData/src/com/company/BestSellinggames.java
@@ -0,0 +1,4 @@
+package com.company;
+
+public class BestSellinggames extends BestSelling {
+}
