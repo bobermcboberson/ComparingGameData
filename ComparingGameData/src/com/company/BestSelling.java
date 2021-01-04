309391b40a185c81598b8627585c077d8ca47250
bobermcboberson <tf2spyman5@gmail.com>
2021-01-04 14:06:19 -0500

    Added Constructor to Superclass

--- ComparingGameData/src/com/company/BestSelling.java
+++ ComparingGameData/src/com/company/BestSelling.java
@@ -8,4 +8,11 @@ public class BestSelling {
     private String releaseDate;
     private String creator;
 
+    // Constructor
+    public BestSelling(String name, int unitSold, String releaseDate, String creator) {
+        this.name = name;
+        this.unitSold = unitSold;
+        this.releaseDate = releaseDate;
+        this.creator = creator;
+    }
 }
