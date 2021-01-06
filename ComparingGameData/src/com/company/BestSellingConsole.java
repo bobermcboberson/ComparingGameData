d33f981c7ddf7dd91fad6f7bf720950c7610f905
fudaylhopkins <Hopkinsfudayl@gmail.com>
2021-01-06 09:31:51 -0500

    added setter and getter

--- ComparingGameData/src/com/company/BestSellingConsole.java
+++ ComparingGameData/src/com/company/BestSellingConsole.java
@@ -2,7 +2,31 @@ package com.company;
 
 public class BestSellingConsole extends BestSelling {
 
+    private String ConsoleName;
     private String BestSellingGame;
 
+    public String getConsoleName() {
+        return ConsoleName;
+    }
 
+    public void setConsoleName(String consoleName) {
+        ConsoleName = consoleName;
+    }
+
+    public String getBestSellingGame() {
+        return BestSellingGame;
+    }
+
+    public void setBestSellingGame(String bestSellingGame) {
+        BestSellingGame = bestSellingGame;
+    }
+
+    public BestSellingConsole(String name, int unitSold, String releaseDate, String creator, String consoleName, String bestSellingGame) {
+        super(name, unitSold, releaseDate, creator);
+        ConsoleName = consoleName;
+        BestSellingGame = bestSellingGame;
+
+
+
+    }
 }
